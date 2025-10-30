65b61aa098ecc87cda7c43c6ce6b86c97aa42c5e
barisc3tin <bariscetintr@gmail.com>
2025-10-30 14:02:13 +0300

    test

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/ceasar-cipher-lab/',
+  base: '/caesar-cipher-lab/',
   build: {
     rollupOptions: {
       output: {
